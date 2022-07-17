6b48e93a756a51bbc5c0ef787ec6957ba09e7f5b
Hugo F Hernandez <97071972+HugoHern@users.noreply.github.com>
2022-07-16 20:39:04 -0400

    Added basic server

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Milestone2-Project-Backend
+# Milestone2-Project-BackEnd
+
+Run NPM install to install dependencies for project
+
+GET	/	The home page
+GET	/profile	Index page listing all profies
+GET	/profile/new	New form for a profile
+POST	/profile	Create a new profie
+GET	/profile/:id	Show one profile in detail (gender, age, likes)
+GET	/profile/:id/edit	Edit form for the profile
+PUT	/profile/:id	Make changes to existing profile
+DELETE	/profile/:id	Delete a profile
+
+
+********* EXTRA ***************************
+POST	/places/:id/comment	Add comment
+DELETE	/places/:id/comment/:commentId	Delete a comment
---
+++ server.js
@@ -0,0 +1,41 @@
+//                        DEPENDENCIES
+const express = require('express') //importing express js library
+const app = express() //assigning express package to be used
+const port = 3000 // will run on localhost:3000
+
+//                      MIDDLEWARE
+app.use(express.static('public')) //serve static files such as images, css files, and javascript files
+
+//                       ROUTES
+
+//Landing page GET request----  localhost:3000 on your local machine
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+//GET request to /user route
+app.get('/user', (req, res) => {
+    res.send('DATING-APP!')
+})
+
+//Respond to POST request on the landing page/homepage
+app.post('/', (req, res) => {
+    console.log('sent a post')
+    res.send('Got a POST request')
+})
+
+//RESPOND TO PUT request to the /user route
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request')
+    console.log('sent a put')
+})
+
+//Respond to a DELETE request to the /user route
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+    console.log('sent a delete')
+})
+
+//Calling listen method to connect to local machine's port
+app.listen(port, () => {
+    console.log(`running server on port ${port}`)
+})
